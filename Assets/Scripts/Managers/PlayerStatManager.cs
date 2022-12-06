bdde61c4aba324b083027aeb249130900db258ce
ARAPHELLE <araphelle.dev@gmail.com>
2022-12-06 11:40:14 -0500

    started on player stats for money shit

---
+++ Assets/Scripts/Managers/PlayerStatManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerStats
+{
+    public int cash;
+}
+
+public class PlayerStatManager : MonoBehaviour
+{
+    public PlayerStats stats;
+}
