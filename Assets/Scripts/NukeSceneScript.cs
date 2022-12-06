2896f31537aa2add8868ce5f623aeba48f806e7e
Rhys G <72167211+Rhys-Grover@users.noreply.github.com>
2022-12-05 22:25:57 -0500

    NUKE!
    like putin!

---
+++ Assets/Scripts/NukeSceneScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NukeSceneScript : MonoBehaviour
+{
+    public GameObject nukeVFX;
+    public AudioClip nukeSFX;
+    public GameObject spawnNuke;
+
+    //private AudioSource audioSource;
+
+    void Start()
+    {
+        //audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.uKey.wasPressedThisFrame)
+        {
+            Nuke();
+        }        
+    }
+
+    void Nuke()
+    {
+        AudioManager.Play(new Audio("Nuke").SetPosition(transform.position).SetParent(transform));
+        GameObject vfxNuke = Instantiate(nukeVFX, spawnNuke.transform.position, spawnNuke.transform.rotation) as GameObject;
+        vfxNuke.transform.localScale = new Vector3(6, 6, 6);
+        Destroy(vfxNuke, 15);
+    }
+}
